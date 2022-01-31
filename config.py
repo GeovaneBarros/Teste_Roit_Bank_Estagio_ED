b02ad32f012d3be4fccd9623bdd01a6ac5e711c9
Geovane <francisco.barros@dellead.com>
2022-01-31 13:49:31 -0300

    Configurando algumas variáveis globais

---
+++ config.py
@@ -0,0 +1,13 @@
+COLUMNS_CNPJ_EMPRESA = ['cnpj', 
+                        'razao_social', 
+                        'natureza_juridica', 
+                        'qualificacao_do_responsavel',
+                        'capital_social_da_empresa', 
+                        'porte_da_empresa', 
+                        'ente_federativo_responsavel']
+
+URL = 'https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj'
+
+FILETYPE_EMPRESA = 'EMPRECSV.zip'
+FILETYPE_ESTABELECIMENTO = 'ESTABELE.zip'
+FILETYPE_SOCIO = 'SOCIOCSV.zip'
